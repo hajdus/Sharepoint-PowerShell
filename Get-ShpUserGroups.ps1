bc2332093ff7aec36d76125c82186827d00d3864
hajdus <32811798+hajdus@users.noreply.github.com>
2018-05-11 14:02:47 +0200

    Create Get-ShpUserGroups.ps1

---
+++ Get-ShpUserGroups.ps1
@@ -0,0 +1,24 @@
+<# 
+    .SYNOPSIS 
+        Get all shp groups for all users 
+ 
+    .DESCRIPTION 
+        To run this script you need to have acces to sharepoint database. Can chceck it with Get-SPContentDatabase. To add permission Add-SPShellAdmin -UserName GPEC\milo -database database_id
+             
+    .OUTPUTS 
+        Txt file save in current user desktop
+ 
+#>
+
+
+if((Get-PSSnapin "Microsoft.SharePoint.PowerShell") -eq $null)
+{
+      Add-PSSnapin Microsoft.SharePoint.PowerShell
+}
+
+$profilepath = (Get-ChildItem env:\userprofile).value 
+$filepath = $profilepath+"\desktop\UserAccessReport.txt"
+
+$urlweb = "http://shpsite.domain.corp"
+Get-SPUser -web $urlweb -Limit All | 
+foreach {$_.Name; $_.Groups | select Name } | Out-File $filepath
